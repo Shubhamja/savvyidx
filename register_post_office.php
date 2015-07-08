3d3503429c62e041e6eb23c30dbedb3797191998
Shubhamja <sales@webcrayons.biz>
2015-07-08 18:06:28 +0530

    Update register_post_office.php

--- register_post_office.php
+++ register_post_office.php
@@ -1,6 +1,5 @@
 <?php 
 function register_cpt_office() {
- 
     $labels = array(
         'name' => _x( 'Office', 'office' ),
         'singular_name' => _x( 'Office', 'office' ),
@@ -15,7 +14,6 @@ function register_cpt_office() {
         'parent_item_colon' => _x( 'Office:', 'office' ),
         'menu_name' => _x( 'Offices', 'office' ),
     );
- 
     $args = array(
         'labels' => $labels,
         'hierarchical' => true,
@@ -36,37 +34,25 @@ function register_cpt_office() {
         'rewrite' => true,
         'capability_type' => 'post'
     );
- 
     register_post_type( 'office', $args );
 }
-
-
 // Add the office address Meta Boxes
-
 function add_office_metaboxes() {
-
     add_meta_box('wpt_office_address', 'Office Details', 'wpt_office_address', 'office', 'normal', 'high');
 }
 // Add the office phone number Meta Boxes
-
-
  // The office Address Metabox
-
 function wpt_office_address() {    
 	global $post;
-	
 	// Noncename needed to verify where the data originated
 	echo '<input type="hidden" name="eventmeta_noncename" id="eventmeta_noncename" value="' . 
 	wp_create_nonce( plugin_basename(__FILE__) ) . '" />';
-	
 	// Get the location data if its already been entered
 	$location = get_post_meta($post->ID, '_location', true);
 	$phn=get_post_meta($post->ID, '_phn', true);
 	$fax=get_post_meta($post->ID, '_fax', true);
 	$gnemail=get_post_meta($post->ID, '_genemail', true);
 	$alogo=get_post_meta($post->ID, '_alogo', true);
-	
-	
 	// Echo out the field
 	?>
 	<div>
@@ -88,41 +74,31 @@ function wpt_office_address() {
 <label><b>Office Email Address</b></label>
 	<input type="email"  name="_genemail" value="<?php echo $gnemail ?> " class="widefat" required>
 </div>
-
 <div>
 </br>
 <label><b>Agent Logo URL</b></label>
 	<input type="text"  name="_alogo" value="<?php echo $alogo?>" class="widefat">
 </div>
-
 <?php
 }
 // Save the Metabox Address
-
 function wpt_save_office_address_meta($post_id, $post) {
-	
 	// verify this came from the our screen and with proper authorization,
 	// because save_post can be triggered at other times
 	if ( !wp_verify_nonce( $_POST['eventmeta_noncename'], plugin_basename(__FILE__) )) {
 	return $post->ID;
 	}
-
 	// Is the user allowed to edit the post or page?
 	if ( !current_user_can( 'edit_post', $post->ID ))
 		return $post->ID;
-
 	// OK, we're authenticated: we need to find and save the data
 	// We'll put it into an array to make it easier to loop though.
-	
 	$events_meta['_location'] = $_POST['_location'];
 	$events_meta['_phn'] = $_POST['_phn'];
 	$events_meta['_fax'] = $_POST['_fax'];
 	$events_meta['_genemail'] = $_POST['_genemail'];
 	$events_meta['_alogo'] = $_POST['_alogo'];
-
-
 	// Add values of $events_meta as custom fields
-	
 	foreach ($events_meta as $key => $value) { // Cycle through the $events_meta array!
 	//	if( $post->post_type == 'revision' ) return; // Don't store custom data twice
 		$value = implode(',', (array)$value); // If $value is an array, make it a CSV (unlikely)
@@ -133,15 +109,9 @@ function wpt_save_office_address_meta($post_id, $post) {
 		}
 		if(!$value) delete_post_meta($post->ID, $key); // Delete if blank
 	}
-
 }
-
-
 add_action('save_post', 'wpt_save_office_address_meta', 1, 2); // save the custom fields 
 //add_action('save_post', 'wpt_save_office_phone_meta', 1, 2); // save the custom fields 
- 
- 
 add_action( 'init', 'register_cpt_office' );
 add_action( 'add_meta_boxes', 'add_office_metaboxes' );
-
 ?>
